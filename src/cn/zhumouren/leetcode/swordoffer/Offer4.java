5e8510d4cb3baed03732cfb8925c1c103c162586
mouren Zhu <1207743591@qq.com>
2023-02-09 12:15:46 +0800

    feat: 完成 剑指offer 题4
    完成 剑指offer 题4 --二维数组中的查找

---
+++ src/cn/zhumouren/leetcode/swordoffer/Offer4.java
@@ -0,0 +1,39 @@
+package cn.zhumouren.leetcode.swordoffer;
+
+public class Offer4 {
+    public static void main(String[] args) {
+        System.out.println(findNumberIn2DArray(new int[][]{
+                {1, 4, 7, 11, 15},
+                {2, 5, 8, 12, 19},
+                {3, 6, 9, 16, 22},
+                {10, 13, 14, 17, 24},
+                {18, 21, 23, 26, 30}
+        }, 55));
+    }
+
+    /**
+     * 在一个 n * m 的二维数组中，每一行都按照从左到右 非递减 的顺序排序，每一列都按照从上到下 非递减 的顺序排序。
+     * 请完成一个高效的函数，输入这样的一个二维数组和一个整数，判断数组中是否含有该整数。
+     *
+     * @param matrix
+     * @param target
+     * @return
+     */
+    public static boolean findNumberIn2DArray(int[][] matrix, int target) {
+        if (matrix.length == 0) {
+            return false;
+        }
+        int x = 0;
+        int y = matrix[0].length - 1;
+        while (x < matrix.length && y >= 0) {
+            if (matrix[x][y] == target) {
+                return true;
+            } else if (matrix[x][y] > target) {
+                y--;
+            } else {
+                x++;
+            }
+        }
+        return false;
+    }
+}
