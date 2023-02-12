93ca85a6205bb4cdbb0c398d8e1584d2ec711e6a
mouren Zhu <1207743591@qq.com>
2023-02-12 12:09:23 +0800

    feat: 完成 剑指offer 题15
    完成 剑指offer 题15 --二进制中1的个数

---
+++ src/cn/zhumouren/leetcode/swordoffer/Offer15.java
@@ -0,0 +1,24 @@
+package cn.zhumouren.leetcode.swordoffer;
+
+public class Offer15 {
+    public static void main(String[] args) {
+        System.out.println(hammingWeight(11));
+        System.out.println(hammingWeight(128));
+        System.out.println(hammingWeight(-3));
+    }
+
+    /**
+     * 编写一个函数，输入是一个无符号整数（以二进制串的形式），返回其二进制表达式中数字位数为 '1' 的个数（也被称为 汉明重量).）。
+     *
+     * @param n
+     * @return
+     */
+    public static int hammingWeight(int n) {
+        int count = 0;
+        while (n != 0) {
+            count++;
+            n &= n - 1;
+        }
+        return count;
+    }
+}
